33f4eb6095522b30d6b6c96b6d3d980d09ee9fd9
YevhenPetrovskyi <71007428+YevhenPetrovskyi@users.noreply.github.com>
2024-01-04 19:57:49 +0200

    bag fix

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 const postList = document.querySelector(".post-list");
 
 fetch("https://jsonplaceholder.typicode.com/posts")
-  .then((response) => response.json())
-  .then((json) => createMarkup(json))
-  .catch((error) => console.log(error));
-
-function createMarkup(posts) {
-  const markup = posts
-    .map(({ title, body }, i) => {
-      return `<li class="post-list-item">
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+  .then((json) => {
+    const markup = json
+      .map(({ title, body }, i) => {
+        return `<li class="post-list-item">
         <span class="post-index">${i + 1}</span>
         <h4 class="post-title">${title}</h4>
         <p class="post-text">${body}</p>
         <a href="#" class="post-link">Read</a>
       </li>`;
-    })
-    .join("");
+      })
+      .join("");
 
-  postList.insertAdjacentHTML("afterbegin", markup);
-}
+    postList.insertAdjacentHTML("afterbegin", markup);
+  })
+  .catch((error) => console.log(error));
 
 function getPostsById(id) {
   const userPosts = {
